cc133141c99ba4124627893bbb410c5466eab80e
Ramon <edraaz199123@gmail.com>
2026-02-15 19:30:47 -0300

    asdasd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+<h1 className="text-3xl font-bold text-green-600">Probando Tailwind</h1>  
+)
 }
 
 export default App
